48d490569f266422ba5b4086764478af8e2439ff
Kagitoshi <santocomet@hotmail.com>
2024-09-11 18:41:21 +0900

    Finishing up challenges in the 14th Chapter.

--- Linked Lists/Singly Linked Lists/main.cpp
+++ Linked Lists/Singly Linked Lists/main.cpp
@@ -24,17 +24,45 @@ class linkedList
 private:
     Node<T>* head;
 
+    void deleteUnknownNode(Node<T>* nodeToDelete)
+    {
+        std::cout << "\n\nDeleting the node...\n";
+
+        if(nodeToDelete->next)
+        {
+            Node<T>* tempNode{nodeToDelete->next};
+
+            nodeToDelete->data = tempNode->data;
+            nodeToDelete->next = tempNode->next;
+
+            delete tempNode;
+        }
+        else
+        {
+            delete nodeToDelete;
+        }
+    }
+
 public:
     linkedList()
     {
         head = nullptr;
     }
 
+    //Data manipulating functions
     void addElementFirst(T dataToAddAtFirstNode);
     void addElementEnd(T dataToAddAtLastNode);
     void addElementAfter(T dataToFind, T dataToAddAfterFoundData);
     void deleteElement(T dataToDelete);
+    void reverseList();
+    void nodeToFindToDelete(T dataToDelete);
+
+
+    //Non-Manipulating functions
     void display();
+    void displayLastNode();
+
+
 };
 /*
     Add a Node with Data to the front of the Linked list.
@@ -196,6 +224,7 @@ void linkedList<T>::deleteElement(T dataToDelete)
             searchForNodeToDelete -> next = searchForNodeToDelete -> next -> next;
 
             delete nodeToDelete;
+            nodeToDelete = nullptr;
             return;
         }
 
@@ -238,6 +267,70 @@ void linkedList<T>::display()
     std::cout << "\n";
 }
 
+template <typename T>
+void linkedList<T>::displayLastNode()
+{
+    Node<T>* currentNode{head};
+
+    while(currentNode->next)
+    {
+        currentNode = currentNode->next;
+    }
+
+    std::cout << "The last node in the list is " << currentNode->data << ".\n\n";
+}
+
+template <typename T>
+void linkedList<T>::reverseList()
+{
+    std::cout << "Starting the reversing of the list...\n";
+
+    Node<T>* currentNode{head};
+    Node<T>* previousNode{nullptr};
+    Node<T>* nextNode{nullptr};
+
+    while(currentNode)
+    {
+        nextNode = currentNode->next;
+        currentNode->next = previousNode;
+        previousNode = currentNode;
+        currentNode = nextNode;
+    }
+
+    head = previousNode;
+
+    std::cout << "... The list has been reversed.\n\n";
+}
+
+template <typename T>
+void linkedList<T>::nodeToFindToDelete(T dataToDelete)
+{
+    Node<T>* currentNode{head};
+
+
+    while(currentNode && currentNode->data != dataToDelete)
+    {
+        currentNode = currentNode->next;
+    }
+
+    if(currentNode)
+    {
+        std::cout << "Found the node with the data \"" << dataToDelete << "\".\n";
+
+        deleteUnknownNode(currentNode);
+
+        std::cout << "\n...The node containing the data \"" << dataToDelete << "\" has been deleted.\n\n";
+        return;
+    }
+    else
+    {
+        std::cout << "A node containing the data \"" << dataToDelete << "\" could not be found.\n"
+                  << "Nothing was deleted.\n\n";
+        return;
+    }
+
+
+}
 
 int main()
 {
@@ -299,6 +392,53 @@ int main()
     std::cout << "That did it! The color pink is no longer in the list and it has been properly deleted.\n"
               << "If it was not properly deleted, the node with the color pink will no free up the memory\n"
               << "that it is using back to the OS. This will cause a memory leak in the application.\n\n"
+              << "Time for a few challenges. Let's do an easy one first. Let's print the last node only.\n"
+              << "So, if done correctly, it should print \"violet\".\n\n";
+
+    colors.displayLastNode();
+
+    std::cout << "Awesome. Let's kick up a notch. Let us reverse the list. So, currently the head node's data\n"
+              << " contains the string \"red\" and from there it goes through the list until it hits \n"
+              << "\"violet\". Well, let's make \"violet\" the head and it will go through the list until\n"
+              << "it hits \"red\". We will print it after we reverse it to ensure everything works.\n\n";
+
+    colors.reverseList();
+
+    std::cout << "Now, the list has been reversed. Let us print it out to see if it worked.\n\n";
+
+    colors.display();
+
+    std::cout << "That worked beautifully it seems. Now, for the final challenge!\n\n"
+              << "Say we were given a node from the middle of the list. Also, let's assume we\n"
+              << "don't know how many nodes in the list, we don't know the head, or anything.\n"
+              << "How can we delete, that node while preserving the list?\n"
+              << "Fun challenge, huh? Easier than you might think. Let's first add a node to the list.\n"
+              << "Let's add the color \"silver\" and we will put after green for now.\n\n"
+              << "Also, don't forget we reversed our list.\n";
+
+    colors.addElementAfter("green", "silver");
+
+    std::cout << "Let's display to see it was added correctly.\n\n";
+
+    colors.display();
+
+    std::cout << "Now, we see it there. Time to play pretend. Let us delete silver...\n"
+              << "Without know we are deleting silver. So we will just write a function that takes in\n"
+              << "\"data\" as an argument and delete it. Since, we can iterate through the list, we will\n"
+              << "need to be careful how to handle the data. Making sure not to point to wrong nodes,\n"
+              << "ensuring the head node is pointing to a node still, and of course actually deleting\n"
+              << "the node to prevent memory leaks.\n\n"
+              << "Let's get started!!!\n\n"
+              << "Let's first write a function to get the node containing \"silver\" so we can pass it\n"
+              << "as an argument.\n";
+
+    colors.nodeToFindToDelete("silver");
+
+    std::cout << "Now that the data has been deleted. Let's displa the list to see if \"silver\" has been removed.\n\n";
+
+    colors.display();
+
+    std::cout << "HUZZAH! We are now done. That was interesting.\n"
               << "This concludes the linked lists introduction. Next we will explore a doubly linked list.\n\n";
 
     return 0;
